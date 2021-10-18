0d02f4d3739dea4ecc146f795c2784d480f519f5
Sophia Barrett <sophia@yoursummit.media>
2021-10-18 16:16:47 -0700

    refactored option for including uvi

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,10 @@ var searchFormEl = document.querySelector("#search-form");
 var searchInputEl = document.querySelector("#search-input")
 var weatherReportEl = document.querySelector("#weather-report");
 
-
-var displayWeatherData = function(data, el) {
-    // display date
+// populate specified weather data into specified element 
+var populateWeatherData = function(data, el, options) {
+    console.log(options);
+    // populate date
     var dateEl = document.createElement("h3");
     var date = new Date(data.dt * 1000);
     dateEl.textContent = date.toLocaleDateString("en-US", {
@@ -16,13 +17,13 @@ var displayWeatherData = function(data, el) {
      });
      el.appendChild(dateEl);
 
-    // display weather icon
+    // populate weather icon
     var iconEl = document.createElement("img");
     iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
     iconEl.setAttribute("alt", data.weather[0].description);
     el.appendChild(iconEl);
 
-    // display temp
+    // populate temperature
     var tempEl = document.createElement("p");
     var temp = data.temp.day;
     // if temp not set, get from outside array instead
@@ -30,21 +31,45 @@ var displayWeatherData = function(data, el) {
     tempEl.textContent = "Temp: " + Math.round(temp) + "°";
     el.appendChild(tempEl);
 
-    // display humidity
+    // populate humidity
     var humidityEl = document.createElement("p");
     humidityEl.textContent = "Humidity: " + Math.round(data.humidity) + "%";
     el.appendChild(humidityEl);
 
-    // display wind speed
+    // populate wind speed
     var windEl = document.createElement("p");
-    windEl.textContent = "Wind : " + data.wind_speed + " MPH";
+    windEl.textContent = "Wind: " + data.wind_speed + " MPH";
     el.appendChild(windEl);
+
+    // check if options set to include uvi
+    if (options && options.includes("include uvi")) {
+        // store uv index in variable
+        var uvi = data.uvi;
+        // create el for uv index
+        var uvEl = document.createElement("p");
+        uvEl.textContent = "UV Index: ";
+        // create span for current UV Index
+        var uvSpan = document.createElement("span");
+        uvSpan.classList = "px-2 py-1 rounded text-white";
+        uvSpan.textContent = uvi;
+        // color code span
+        if (uvi <= 2) {
+            uvSpan.classList.add("bg-success");
+        } else if (uvi <= 5) {
+            uvSpan.classList.add("bg-warning");
+        } else {
+            uvSpan.classList.add("bg-danger");
+        }
+        uvEl.appendChild(uvSpan);
+        // append UV Index to current weather only
+        el.appendChild(uvEl);
+    }
 }
 
 // update weather dashboard with fetched city and weather data
 var updateDashboard = function(cityData, weatherData) {
     console.log(cityData, weatherData);
-    // clear weather report
+    // clear weather report el
     weatherReportEl.innerHTML = "";
 
     // create current weather el
@@ -52,7 +77,7 @@ var updateDashboard = function(cityData, weatherData) {
     currentWeatherEl.setAttribute("id", "current-weather");
     currentWeatherEl.classList = "row";
 
-    // display city name
+    // populate city name
     var cityNameEl = document.createElement("h2");
     // check if state is set
     if (cityData.state) {
@@ -60,32 +85,14 @@ var updateDashboard = function(cityData, weatherData) {
     } else {
         cityNameEl.textContent = cityData.name + ", " + cityData.country;
     }
+    // append city name to current weather el
     currentWeatherEl.appendChild(cityNameEl);
 
-    // display current weather data
-    displayWeatherData(weatherData.current, currentWeatherEl);
-
-    // create el for current UV Index
-    var uvEl = document.createElement("p");
-    uvEl.innerHTML = "UV Index: ";
-    // create span for current UV Index
-    var uvi = weatherData.current.uvi;
-    var uvSpan = document.createElement("span");
-    uvSpan.classList = "px-2 py-1 rounded text-white";
-    uvSpan.textContent = uvi;
-    // color code UV Index
-    if (uvi <= 2) {
-        uvSpan.classList.add("bg-success");
-    } else if (uvi <= 5) {
-        uvSpan.classList.add("bg-warning");
-    } else {
-        uvSpan.classList.add("bg-danger");
-    }
-    uvEl.appendChild(uvSpan);
-    // append UV Index to current weather only
-    currentWeatherEl.appendChild(uvEl);
-    
-
+    // populate data into current weather el
+    var currentWeatherOptions = [
+        "include uvi"
+    ];
+    populateWeatherData(weatherData.current, currentWeatherEl, currentWeatherOptions);
     // append current weather to DOM
     weatherReportEl.appendChild(currentWeatherEl);
 
@@ -94,17 +101,16 @@ var updateDashboard = function(cityData, weatherData) {
     forcastEl.setAttribute("id", "forcast");
     forcastEl.classList = "row";
 
-    // display forcased weather for next 5 days
+    // populate forcased weather for next 5 days
     for (var i = 1; i < 5; i++) {
         var singleDayForecastEl = document.createElement("div");
         singleDayForecastEl.classList = "col";
-        displayWeatherData(weatherData.daily[i], singleDayForecastEl);
+        populateWeatherData(weatherData.daily[i], singleDayForecastEl);
         forcastEl.appendChild(singleDayForecastEl);
     }
 
     // append forcast to DOM
-    weatherReportEl.appendChild(forcastEl);
-    
+    weatherReportEl.appendChild(forcastEl);   
 }
 
 // fetch weather data from fetched city data
